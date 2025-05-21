aea6f7927dde7980a5f558adfecebce6ea814ae0
DiVaY ShArMa <115571620+sharmadivay@users.noreply.github.com>
2025-05-21 15:07:56 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 class="">
+    Hello world!
+  </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening at Port : ${PORT}`);
+})
