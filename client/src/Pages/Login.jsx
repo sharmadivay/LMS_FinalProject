77946145b024a9a7b47a1c701057f758d1e48bb7
Atul Sharma <atulsharma02004@gmail.com>
2025-05-26 21:47:01 +0530

    bekar

---
+++ client/src/Pages/Login.jsx
@@ -0,0 +1,13 @@
+function Login(){
+
+
+    return(
+        <div className="flex align-center justify-center h-1/2 w-1/2 bg-amber-500">
+
+           
+            
+        </div>
+    )
+}
+
+export default Login;
