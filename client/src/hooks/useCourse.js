b8be4c8caad45972cb53f6faa66d8e9661b20dc5
TUSHARSHARMA0107 <tushar43189@gmail.com>
2025-06-20 20:00:01 +0530

    upload

---
+++ client/src/hooks/useCourse.js
@@ -0,0 +1,32 @@
+
+import axios from 'axios';
+
+axios.defaults.withCredentials = true; 
+
+const API_BASE_URL = 'http://localhost:8082/api'; 
+
+export const uploadCourse = async (formData) => {
+  const res = await axios.post(`${API_BASE_URL}/createCourse`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+  return res.data;
+};
+
+// ✅ Fetch all courses
+export const fetchCourses = async () => {
+  const res = await axios.get(`${API_BASE_URL}/courses`);
+  return res.data.courses;
+};
+
+// ✅ Update an existing course
+export const updateCourse = async (id, updatedData) => {
+  const res = await axios.put(`${API_BASE_URL}/courses/${id}`, updatedData);
+  return res.data;
+};
+
+export const deleteCourse = async (id) => {
+  const res = await axios.delete(`${API_BASE_URL}/courses/${id}`);
+  return res.data;
+};
